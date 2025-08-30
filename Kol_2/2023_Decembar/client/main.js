827feb76d31aa3d5def2df4cc48a8e9963ac7c89
bxgda <jovan.bogdanovic@elfak.rs>
2025-08-30 19:49:41 +0200

    prepravke

--- Kol_2/2023_Decembar/client/main.js
+++
@@ -1,9 +0,0 @@
-import { vratiProdavnice } from "./utils.js";
-import { Application } from "./application.js";
-
-let prodavnice = await vratiProdavnice();
-
-prodavnice.forEach((prod) => {
-  let app = new Application(prod);
-  app.draw(document.body);
-});
--- Kol_2/2024_Januar/client/main.js
+++
@@ -1,7 +0,0 @@
-import { Application } from "./application.js";
-import { vratiAutomobile } from "./utils.js";
-
-let automobili = await vratiAutomobile();
-
-let app = new Application(automobili);
-app.draw(document.body);
